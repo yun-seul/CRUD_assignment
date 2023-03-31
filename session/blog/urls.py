072936b7af208356d82d9336d70e34a5571ea531
KimChanJin97 <102044895+KimChanJin97@users.noreply.github.com>
2023-03-31 11:18:48 +0900

    fix: 세션진행 중 view craete 수정사항 적용

--- session/blog/urls.py
+++ session/blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.home, name='home'), # 127.0.0.1:8000
     path('blog/<int:blog_id>', views.detail, name='detail'),
     path('blog/new', views.new, name='new'),
     path('blog/create', views.create, name='create'),
--- session/session/settings.py
+++ session/session/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #김도훈님
     'blog',
 ]
 
