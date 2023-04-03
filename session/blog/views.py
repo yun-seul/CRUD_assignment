1cd9b7b00a565a86ba1008f5436fcc33314aaf40
KimChanJin97 <102044895+KimChanJin97@users.noreply.github.com>
2023-04-03 09:56:13 +0900

    주석 변경

--- session/blog/views.py
+++ session/blog/views.py
@@ -14,39 +14,39 @@ def detail(request, blog_id):
 def new(request):
     return render(request,'new.html')
 
-# def create(request):
-#     new_blog = Blog()
-#     new_blog.title = request.POST['title']
-#     new_blog.content = request.POST['content']
-#     new_blog.save()
-#     return redirect('detail', new_blog.id)
-#     return render(request, 'detail.html', {'blog':new_blog})
-
 def create(request):
-    form = BlogForm(request.POST)
+    new_blog = Blog()
+    new_blog.title = request.POST['title']
+    new_blog.content = request.POST['content']
+    new_blog.save()
+    return redirect('detail', new_blog.id)
+    # return render(request, 'detail.html', {'blog':new_blog})
 
-    if form.is_valid():
-        new_blog = form.save(commit=False)
-        new_blog.save()
-        return redirect('detail', new_blog.id)
-
-    return render(request, 'new.html')
+# def create(request):
+#     form = BlogForm(request.POST)
+#
+#     if form.is_valid():
+#         new_blog = form.save(commit=False)
+#         new_blog.save()
+#         return redirect('detail', new_blog.id)
+#
+#     return render(request, 'new.html')
 
 def edit(request, blog_id):
     edit_blog = get_object_or_404(Blog, pk=blog_id) # print() 해보기
     return render(request, 'edit.html', {'edit_blog':edit_blog})
 
 
-# def update(request, blog_id):
-#     old_blog = get_object_or_404(Blog, pk=blog_id)
-#     old_blog.title = request.POST["title"]
-#     old_blog.content = request.POST["content"]
-#     old_blog.save()
-#     return redirect('detail', old_blog.id)
-
 def update(request, blog_id):
     old_blog = get_object_or_404(Blog, pk=blog_id)
-    form = BlogForm(request.POST, instance=old_blog)
+    old_blog.title = request.POST["title"]
+    old_blog.content = request.POST["content"]
+    old_blog.save()
+    return redirect('detail', old_blog.id)
+
+# def update(request, blog_id):
+#     old_blog = get_object_or_404(Blog, pk=blog_id)
+#     form = BlogForm(request.POST, instance=old_blog)
 
     # 클라이언트가 유효한 값을 입력한 경우
     if form.is_valid():
